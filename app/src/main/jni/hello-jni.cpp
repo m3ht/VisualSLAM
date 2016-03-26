7def5b3afab1837baa8a4a4557bd1c6c5748837b
Sudhanva Sreesha <sudhanvas@gmail.com>
2016-03-26 10:17:04 -0400

    Eigen 3 now compiles on Android!

--- app/src/main/jni/hello-jni.cpp
+++ app/src/main/jni/hello-jni.cpp
@@ -1,9 +1,22 @@
-#include <string.h>
 #include <jni.h>
+
+#include <iostream>
+#include <sstream>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+using namespace std;
 
 extern "C" {
 	JNIEXPORT jstring JNICALL
 	Java_edu_umich_eecs_robotics_slam_MainActivity_stringFromJNI(JNIEnv *env, jobject instance) {
-		return env->NewStringUTF("Hello World!");
+		MatrixXd matrix(2, 2);
+		matrix(0, 0) = 1;
+		matrix(0, 1) = 2;
+		matrix(1, 0) = 3;
+		matrix(1, 1) = 4;
+		stringstream ss;
+		ss << "Hello Eigen3! A Matrix: " << matrix << endl;
+		return env->NewStringUTF(ss.str().c_str());
 	}
 }
