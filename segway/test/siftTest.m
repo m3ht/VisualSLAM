20cf89456678a951c3bd16f55da9cfee5563398c
Frederick Wirth <fwirth@umich.edu>
2016-04-04 22:51:46 +0000

    tested siftTest with stereo images

--- segway/test/siftTest.m
+++ segway/test/siftTest.m
@@ -5,7 +5,9 @@
 %mean and variance of these vectors. 
 clear
 close all
-fileNames = dir('*.jpg');
+S = pwd
+S = strcat(S ,'/StereoImages/*.pnm')
+fileNames = dir(S);
 numImages = length(fileNames);
 
 imageName=cell(numImages,1);
@@ -16,8 +18,7 @@ for i=1:numImages
     imageName{i} = fileNames(i).name;
 end
 
-
-
+numImages
 %{
 imageName{1} = 'img_CAMERA1_1261228969.770601_left.jpg';
 imageName{2} = 'img_CAMERA1_1261228969.770601_right.jpg';
@@ -25,23 +26,44 @@ imageName{2} = 'img_CAMERA1_1261228969.770601_right.jpg';
 imageName{3} = 'img_CAMERA1_1261228969.820611_left.jpg';
 %}
 %im=zeros([size(imread(imageName{1})) 3]);
-
-for i=1:3
-img = imread(imageName{i});
-im{i} = im2double(rgb2gray(img));
+tic
+figure
+for i=1:2:numImages
+left = imread(imageName{i});
+im{i} = im2double(left);
 
 
 points = detectSURFFeatures(im{i});
 strongestPoints{i} = points.selectStrongest(50);
 
-figure()
+subplot(1,3,1)
 imshow(im{i}); hold on;
- strongestPoints{i}.plot('showOrientation',true);
+strongestPoints{i}.plot('showOrientation',true);
  features{i}=extractFeatures(im{i}, strongestPoints{i});
-end
-
-
-
-  %[features, valid_points] = 
-
  
+ 
+ 
+right = imread(imageName{i+1});
+im{i+1} = im2double(right);
+
+
+points = detectSURFFeatures(im{i+1});
+strongestPoints{i+1} = points.selectStrongest(50);
+
+subplot(1,3,2)
+imshow(im{i+1}); hold on;
+strongestPoints{i+1}.plot('showOrientation',true);
+ features{i+1}=extractFeatures(im{i+1}, strongestPoints{i+1});
+ 
+ disparityMap = disparity(left, right);
+ subplot(1,3,3)
+imshow(disparityMap, [0, 64]);
+title('Disparity Map');
+colormap jet
+colorbar
+ 
+
+end
+toc
+
+
